444ce0e59e81f94ff88e1de52be4f28d739dfd38
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2023-02-06 16:21:17 +0100

    feat: add commander cli interface with --help and --reporter options

--- bin/tsd-lite.js
+++ bin/tsd-lite.js
@@ -1,50 +1,103 @@
 #!/usr/bin/env node
 
 import fastGlob from "fast-glob"
-import { formatSuiteResult, formatTestResult } from "../lib/formatter.fancy.js"
-import { runTest } from "../lib/fn.runTest.js"
+import { Option, program } from "commander"
 
-const globPatterns = process.argv.slice(2)
-const stats = fastGlob
-  .sync(globPatterns, { absolute: true })
-  .map(absolutePath => {
-    const diagnosis = runTest(absolutePath)
-    const output = formatTestResult(diagnosis)
+import tapReporter from "../lib/reporters/tap.js"
+import fancyReporter from "../lib/reporters/fancy.js"
 
-    if (diagnosis.errors.length === 0) {
-      process.stdout.write(`${output}\n`)
-    } else {
-      process.stderr.write(`${output}\n`)
-    }
+import { getNodeVersion, getPackageInfo } from "../lib/utils/node.js"
+import { green, red } from "../lib/utils/colors.js"
+import { runSuite } from "../lib/runSuite.js"
 
-    return diagnosis
-  })
-  // eslint-disable-next-line unicorn/no-array-reduce
-  .reduce(
-    (acc, diagnosis) => {
-      if (diagnosis.errors.length === 0) {
-        acc.passCount += 1
-      } else {
-        acc.failCount += 1
-      }
-      acc.duration = [
-        acc.duration[0] + diagnosis.duration[0],
-        acc.duration[1] + diagnosis.duration[1],
-      ]
+/**
+ * Node.js version guard
+ */
 
-      return acc
-    },
-    {
-      passCount: 0,
-      failCount: 0,
-      duration: /** @type {[number, number]} */ ([0, 0]),
-    }
+const required = 16
+const [major] = getNodeVersion()
+
+if (major < required) {
+  process.stderr.write(
+    `You are running Node ${red.fg(
+      `v${major}`
+    )}, tsd-lite requires Node ${green.fg(`v${required}`)} or higher.`
   )
 
-process.stdout.write(`\n${formatSuiteResult(stats)}\n`)
-
-if (stats.failCount > 0) {
   process.exit(1)
 }
 
-process.exit(0)
+/**
+ * CLI specific logic via "commander"
+ */
+
+const { name, description, version } = await getPackageInfo()
+
+program
+  .name(name)
+  .description(description)
+  .version(version, "-v, --version", "Display version number")
+  .helpOption("-h, --help", "Display this help guide")
+  .showSuggestionAfterError()
+  .addOption(
+    new Option("-r, --reporter <name>", "Print results using reporter")
+      .default("tap")
+      .choices(["tap", "fancy"])
+  )
+  .argument("globPatterns...", "Typing test files glob patterns")
+  .action(async (globPatterns, { reporter: reporterName }) => {
+    const reporter = reporterName === "tap" ? tapReporter : fancyReporter
+    const files = fastGlob.sync(globPatterns, { absolute: true })
+
+    process.stdout.write(
+      `${reporter.formatIntro({
+        count: files.length,
+        description: program.description(),
+      })}\n`
+    )
+
+    runSuite(files, {
+      onTestFinish: (result, index) => {
+        process.stdout.write(
+          `${reporter.formatTest({
+            index,
+            result,
+          })}\n`
+        )
+      },
+      onSuiteFinish: results => {
+        // eslint-disable-next-line unicorn/no-array-reduce
+        const stats = results.reduce(
+          (acc, testResult) => {
+            if (testResult.errors.length === 0) {
+              acc.passCount += 1
+            } else {
+              acc.failCount += 1
+            }
+
+            acc.duration = [
+              acc.duration[0] + testResult.duration[0],
+              acc.duration[1] + testResult.duration[1],
+            ]
+
+            return acc
+          },
+          {
+            passCount: 0,
+            failCount: 0,
+            duration: /** @type {[number, number]} */ ([0, 0]),
+          }
+        )
+
+        process.stdout.write(`\n${reporter.formatSuite(stats)}\n`)
+
+        if (stats.failCount > 0) {
+          process.exit(1)
+        }
+
+        process.exit(0)
+      },
+    })
+  })
+
+program.parse(process.argv)
